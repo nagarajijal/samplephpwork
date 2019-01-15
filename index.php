61a28854f8a7258007745ba55e5d59054d686d78
nagarajijal <nagaraj.jul29@gmail.com>
2019-01-15 17:34:45 +0530

    Update index.php
    The content is changed with page background color, the nature image and the content.

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 
 <h1>Developing PHP Application for DevOps</h1>
 
- <h2>This got reflected automatically once the php was edited in GitHub Repo</h2>
- 
+ <h2>The background color, the below image and this line content automatically got reflected once the commit is done in GitHub</h2>
+ <img scr = "https://www.planwallpaper.com/static/images/Beautiful-Scenery-HD-Wallpapers-Pictures-11.jpg">
 <?php
-echo "version :Ver 1.0";
+echo "version :Ver 2.0";
 ?>
 
 </body>
