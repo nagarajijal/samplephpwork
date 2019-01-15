53c8c6fbadd553b55c5a105abe3296a315581bbf
nagarajijal <nagaraj.jul29@gmail.com>
2019-01-15 17:43:02 +0530

    Update index.php
    Changed couple of contents

--- index.php
+++ index.php
@@ -3,14 +3,16 @@
 <body bgcolor = 'wheat'>
 
 <h1>Developing PHP Application for DevOps</h1>
- 
+<br/>
+ <h3> The code pipeline is established between ElasticBean and GitHub</h3> <br/>
 <br/>
  <h2><font color="maroon">The background color, the below image and this line content automatically got reflected once the commit is done in GitHub </font></h2>
  <br/>
  <img src = "https://www.planwallpaper.com/static/images/Beautiful-Scenery-HD-Wallpapers-Pictures-11.jpg">
+ <br/>
 <?php
 echo "version :Ver 2.0";
 ?>
-
+<h4><i>Nagaraj ~ </i></h4>
 </body>
 </html>
