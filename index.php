95db7e9aef247eb292ba96b81040551fd18f252f
nagarajijal <nagaraj.jul29@gmail.com>
2019-01-15 17:28:10 +0530

    Update index.php
    Added few things to the php

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <!
 <html>
-<body>
+<body bgcolor = 'wheat'>
 
 <h1>Developing PHP Application for DevOps</h1>
 
+ <h2>This got reflected automatically once the php was edited in GitHub Repo</h2>
+ 
 <?php
 echo "version :Ver 1.0";
 ?>
