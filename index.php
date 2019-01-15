aa9ac6449432e12721e2fe7baff0f7bb15438fd0
nagarajijal <nagaraj.jul29@gmail.com>
2019-01-15 17:38:05 +0530

    Update index.php
    The font color changed

--- index.php
+++ index.php
@@ -3,8 +3,10 @@
 <body bgcolor = 'wheat'>
 
 <h1>Developing PHP Application for DevOps</h1>
-
- <h2>The background color, the below image and this line content automatically got reflected once the commit is done in GitHub</h2>
+ 
+<br/>
+ <h2 color = "green">The background color, the below image and this line content automatically got reflected once the commit is done in GitHub</h2>
+ <br/>
  <img src = "https://www.planwallpaper.com/static/images/Beautiful-Scenery-HD-Wallpapers-Pictures-11.jpg">
 <?php
 echo "version :Ver 2.0";
